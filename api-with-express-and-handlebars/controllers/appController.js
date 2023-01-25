a763d48282f656f32801697803271408cd68a62a
AndrewMarcondes <drewmarcondes@gmail.com>
2023-01-24 21:39:26 -0600

    added the nodejs server

---
+++ api-with-express-and-handlebars/controllers/appController.js
@@ -0,0 +1,103 @@
+const bent = require('bent')
+const getJSON = bent('json')
+const semverMajor = require('semver/functions/major')
+const semverGt = require('semver/functions/gt')
+const packageJson = require('../package.json')
+const NODE_API_URL = 'https://nodejs.org/dist/index.json'
+const MANGA_DEX_URL = 'https://api.consumet.org/manga/mangadex/'
+const MANGA_DEX_INFO_URL = 'https://api.consumet.org/manga/mangadex/info/'
+
+const isGrater = (a, b) => semverGt(a.version, b.version)
+
+const getLatestReleases = (releases) =>
+  releases.reduce((acc, release) => {
+    const major = `v${semverMajor(release.version)}`
+    const existing = acc[major]
+    if (!existing || isGrater(release, existing)) {
+      acc[major] = release
+    }
+    return acc
+  }, {})
+
+exports.dependencies = (req, res) => {
+  const dependencies = Object.entries(
+    packageJson.dependencies
+  ).map(([key, value]) => ({ name: key, version: value }))
+  res.render('dependencies.hbs', { dependencies })
+}
+
+exports.minimumSecurePage = async (req, res) => {
+  const releases = await getJSON(NODE_API_URL)
+  const securedReleases = releases.filter((release) => release.security)
+  const minimumSecured = getLatestReleases(securedReleases)
+  res.render('minimum-secure.hbs', {
+    result: JSON.stringify(minimumSecured, undefined, '  ')
+  })
+}
+
+exports.latestReleasesPage = async (req, res) => {
+  const releases = await getJSON(NODE_API_URL)
+  const latest = getLatestReleases(releases)
+  res.render('latest-releases.hbs', {
+    result: JSON.stringify(latest, undefined, '  ')
+  })
+}
+
+exports.minimumSecure = async (req, res) => {
+  try {
+    res.setHeader('Content-type', 'application/json')
+    const releases = await getJSON(NODE_API_URL)
+    const securedReleases = releases.filter((release) => release.security)
+    const minimumSecured = getLatestReleases(securedReleases)
+    res.json(minimumSecured)
+  } catch (error) {
+    res.json({ error, message: `Unable to fetch data on ${req.route.path}` })
+  }
+}
+
+exports.latestReleases = async (req, res) => {
+  try {
+    res.setHeader('Content-type', 'application/json')
+    const releases = await getJSON(NODE_API_URL)
+    res.json(getLatestReleases(releases))
+  } catch (error) {
+    res.json({ error, message: `Unable to fetch data on ${req.route.path}` })
+  }
+}
+
+exports.home = (req, res) => {
+  res.render('home.hbs')
+}
+
+exports.manga = async (req, res) => {
+  try {
+    res.setHeader('Content-type', 'application/json')
+    res.json('hello world')
+  } catch (error) {
+    res.json({ error, message: `Unable to fetch data on ${req.route.path}` })
+  }
+}
+
+exports.mangaInformation = async (req, res) => {
+  try {
+    res.setHeader('Content-type', 'application/json')
+
+    const MANGA_NAME = req.params.mangaName.toLowerCase().replace(/-/g, " ")
+
+    const getMangas = await getJSON(MANGA_DEX_URL+MANGA_NAME)
+
+    let bestMatch
+
+    getMangas.results.forEach(result => {
+      if(result.title.toLowerCase() == MANGA_NAME){
+        bestMatch = result
+      }
+    })
+
+    const getMangaInfo = await getJSON(MANGA_DEX_INFO_URL+bestMatch.id)
+    res.json(getMangaInfo)
+  } catch (error) {
+    res.json({ error, message: `Unable to fetch data on ${req.route.path}` })
+  }
+}
+
---
+++ api-with-express-and-handlebars/routes/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+const appController = require('../controllers/appController.js')
+
+// Application Routes
+router.get('/', appController.home)
+router.get('/dependencies', appController.dependencies)
+router.get('/minimumSecure', appController.minimumSecurePage)
+router.get('/latestReleases', appController.latestReleasesPage)
+
+// API Routes
+router.get('/api/minimum-secure', appController.minimumSecure)
+router.get('/api/latest-releases', appController.latestReleases)
+
+// Manga Read Routes
+router.get('/manga', appController.manga)
+router.get('/manga/info/:mangaName', appController.mangaInformation)
+
+module.exports = router
